b4f38f2233397d400479c0c5d66b8b5333acf53a
Corey Light <coreylight@gmail.com>
2018-03-02 08:26:56 -0600

    fixes #55

--- src/handlerCode.js
+++ src/handlerCode.js
@@ -1,6 +1,9 @@
 exports['EXPORT_NAME'] = function FUNCTION_NAME(event, context, callback) {
   try {
-    return iopipe((evt, ctx) => require('./RELATIVE_PATH').METHOD(evt, ctx))(event, context, callback);
+    return iopipe((evt, ctx, cb) => {
+      const mod = require('./RELATIVE_PATH');
+      return mod.METHOD(evt, ctx, cb);
+    })(event, context, callback);
   } catch (err) {
     throw err;
   }
--- testProjects/default/index.test.js
+++ testProjects/default/index.test.js
@@ -1,6 +1,9 @@
 import _ from 'lodash';
 import AdmZip from 'adm-zip';
 
+process.env.IOPIPE_TOKEN = 'test_token';
+process.env.IOPIPE_DEBUG = true;
+
 test('Generated file requires plugin and includes plugin inline', async () => {
   const zip = new AdmZip('./.serverless/sls-unit-test-default.zip');
   expect(1).toBe(1);
@@ -16,4 +19,8 @@ test('Generated file requires plugin and includes plugin inline', async () => {
     exports.simple({}, { succeed });
   });
   expect(result.statusCode).toEqual(200);
+  const syntaxErrorResult = await new Promise(succeed => {
+    exports.syntaxError({}, {}, succeed);
+  });
+  console.log(syntaxErrorResult);
 });
