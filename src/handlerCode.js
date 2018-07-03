db8b54bdd3d03888a23539ece145119da37dc266
Corey Light <coreylight@gmail.com>
2018-07-03 14:47:29 -0500

    fix(handler files): move require block outside of handler for best lambda cold start performance
    By moving the require block outside of the handler, AWS can properly use performance enhancements on
    their side to optimize the cold start.
    
    fix #77

--- src/handlerCode.js
+++ src/handlerCode.js
@@ -1,7 +1,17 @@
+let handler, handlerError;
+try {
+  handler = require('../RELATIVE_PATH');
+} catch (err) {
+  handlerError = err;
+}
+
 exports['EXPORT_NAME'] = function FUNCTION_NAME(event, context, callback) {
   try {
     return iopipe((evt, ctx, cb) => {
-      return require('../RELATIVE_PATH').METHOD(evt, ctx, cb);
+      if (handlerError) {
+        return cb(handlerError);
+      }
+      return handler.METHOD(evt, ctx, cb);
     })(event, context, callback);
   } catch (err) {
     throw err;
